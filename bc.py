bbfe490fae4cc93b4d8c56c4bcc285e5e10ff433
RRKalakonda <129708478+RRKalakonda@users.noreply.github.com>
2023-04-16 17:53:24 +0530

    8 cases passed one

--- bc.py
+++ bc.py
@@ -311,17 +311,17 @@ def evaluate(node, variables=None):
         return value
     elif node.token.token_type == TokenType.PRINT:
         values = []
-        for child in node.left:
-            try:
+        try:
+            for child in node.left:
                 ans = evaluate(child, variables)
                 print(ans, end=" ")
                 values.append(ans)
-                print()
-                # values = [evaluate(child, variables) for child in node.left]
-            except ZeroDivisionError as e:
-                # print(*values, end=" ")
-                print("divide by zero")
-                sys.exit()
+            print()
+            # values = [evaluate(child, variables) for child in node.left]
+        except ZeroDivisionError as e:
+            # print(*values, end=" ")
+            print("divide by zero")
+            sys.exit()
         
         return values[0] if values else None
 
@@ -349,9 +349,9 @@ def main(program):
                 # print(*values, end=" ")
                 print("divide by zero")
                 sys.exit()
-            except Exception as e:
-                print("parse error")
-                sys.exit()
+            # except Exception as e:
+            #     print("parse error")
+            #     sys.exit()
 
 
 
